bb30626adbd086e56b118b2b147fbf4502973688
alessandro-stella <alessandro.stella2004@gmail.com>
2026-05-20 15:53:02 +0200

    Changed project name in wakatime plugin (testing)

--- lua/plugins/wakatime.lua
+++ lua/plugins/wakatime.lua
@@ -1,1 +1,8 @@
-return { "wakatime/vim-wakatime", lazy = false }
+return {
+	"wakatime/vim-wakatime",
+	lazy = false,
+
+	init = function()
+		vim.g.wakatime_project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+	end,
+}
