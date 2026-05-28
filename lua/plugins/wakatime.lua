ea64abce561655f0c1bf431076c29796e9dcf145
alessandro-stella <alessandro.stella2004@gmail.com>
2026-05-28 18:46:55 +0200

    Removed useless variable from wakatime

--- lua/plugins/wakatime.lua
+++ lua/plugins/wakatime.lua
@@ -1,8 +1,7 @@
 return {
 	"wakatime/vim-wakatime",
 	lazy = false,
-
-	init = function()
-		vim.g.wakatime_project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-	end,
+	opts = {
+		status_bar_enabled = false,
+	},
 }
