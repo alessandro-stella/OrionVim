4f0ad4db30feb06309681c6fd358dbd0c6a3a307
alessandro-stella <alessandro.stella2004@gmail.com>
2026-05-29 09:27:39 +0200

    Changed scopes of custom variabiles

--- lua/configs/autocmds.lua
+++ lua/configs/autocmds.lua
@@ -2,10 +2,10 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "text", "conf", "markdown", "" },
 	callback = function()
-		vim.opt.expandtab = false
-		vim.opt.tabstop = 4
-		vim.opt.shiftwidth = 4
-		vim.opt.softtabstop = 0
+		vim.opt_local.expandtab = false
+		vim.opt_local.tabstop = 4
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.softtabstop = 0
 	end,
 })
 
